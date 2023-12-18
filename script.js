ad8ececea8466408ae2923adc03cf61b79be73d4
DANIEL GIATHI <giathidaniel252@gmail.com>
2023-12-18 17:34:11 +0300

    Flowers

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 onload = () => {
-    const c = setTimeout((
-        document.body.classList.remove("loaded");
-        
-        clearTimeout(c);
-    ))
-}
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+};
